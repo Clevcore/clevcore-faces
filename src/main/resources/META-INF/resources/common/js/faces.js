04b0f5a145d4aa4ea3bbbb00901f11354600e3b3
daniville <daniville1@hotmail.com>
2015-07-29 21:45:36 -0300

    US-78: Remove animations on titles and body

--- src/main/resources/META-INF/resources/common/js/faces.js
+++ src/main/resources/META-INF/resources/common/js/faces.js
@@ -2,35 +2,4 @@
 $(document).ready(function() {
 	jsf.ajax.addOnEvent(HandleAjax.listener);
 	reset();
-	init();
 });
-
-function init() {
-	if (window.addEventListener) {
-		window.addEventListener("beforeunload", function() {
-			remplaceClass("title", "animate-fadeInLeftLarge", "animate-fadeOutLeftLarge");
-		}, false);
-		window.addEventListener("beforeunload", function() {
-			remplaceClass("subtitle", "animate-fadeInRightLarge", "animate-fadeOutRightLarge");
-		}, false);
-		window.addEventListener("beforeunload", function() {
-			remplaceClass("section", "animate-fadeIn", "animate-fadeOut");
-		}, false);
-		window.addEventListener("beforeunload", function() {
-			remplaceClass("footer", "animate-fadeIn", "animate-fadeOut");
-		}, false);
-	} else {
-		window.attachEvent("onbeforeunload", function() {
-			remplaceClass("title", "animate-fadeInLeftLarge", "animate-fadeOutLeftLarge");
-		});
-		window.attachEvent("onbeforeunload", function() {
-			remplaceClass("subtitle", "animate-fadeInRightLarge", "animate-fadeOutRightLarge");
-		});
-		window.attachEvent("onbeforeunload", function() {
-			remplaceClass("section", "animate-fadeIn", "animate-fadeOut");
-		});
-		window.attachEvent("onbeforeunload", function() {
-			remplaceClass("footer", "animate-fadeIn", "animate-fadeOut");
-		});
-	}
-}
