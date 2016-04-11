bb23c5b00736c7e8c7d9d52c3588f509ceff26e6
daniville1 <daniville1@hotmail.com>
2016-04-11 15:38:14 -0300

    Add decimalAdjust to round, floor and ceil number

--- src/main/resources/META-INF/resources/common/js/reset.js
+++ src/main/resources/META-INF/resources/common/js/reset.js
@@ -102,3 +102,42 @@ function getCaret(input) {
 		return sel.text.length - selLen;
 	}
 }
+
+(function() {
+	function decimalAdjust(type, value, exp) {
+		if (typeof exp === 'undefined' || +exp === 0) {
+			return Math[type](value);
+		}
+		
+		value = +value;
+		exp = +exp;
+		
+		if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+			return NaN;
+		}
+		
+		value = value.toString().split('e');
+		value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+		value = value.toString().split('e');
+		
+		return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+	}
+
+	if (!Math.round10) {
+		Math.round10 = function(value, exp) {
+			return decimalAdjust('round', value, exp);
+		};
+	}
+
+	if (!Math.floor10) {
+		Math.floor10 = function(value, exp) {
+			return decimalAdjust('floor', value, exp);
+		};
+	}
+
+	if (!Math.ceil10) {
+		Math.ceil10 = function(value, exp) {
+			return decimalAdjust('ceil', value, exp);
+		};
+	}
+})();
