8e2a9b7c4ff0a1046643ef9bf9500dd63889e6ed
daniville1 <daniville1@hotmail.com>
2016-08-25 13:07:01 -0300

    Add function for sort by key

--- src/main/resources/META-INF/resources/common/js/common.js
+++ src/main/resources/META-INF/resources/common/js/common.js
@@ -336,6 +336,15 @@ function containElement(parent, child) {
 }
 
 // utils
+function sortByKey(array, key) {
+	return array.sort(function(a, b) {
+		var x = a[key];
+		var y = b[key];
+		
+		return x < y ? -1 : (x > y ? 1 : 0);
+	});
+}
+
 function setInputCheckbox(selectors, value) {
 	var elements = getSelectors(selectors);
 
